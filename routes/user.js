56521be6a1496e6cd56df9c8468b1efe7f73b32f
G-ACHE <achegregory@icloud.com>
2021-05-17 12:01:32 +0200

    rajout middleware dans une route

--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,12 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
+const auth = require("../middleware/auth");
+
 // routes post pour l inscription et le login, et get pour le masquage des données personnelles
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
-router.get("/me", userCtrl.me);
+router.get("/me", auth, userCtrl.me);
 
 module.exports = router;
